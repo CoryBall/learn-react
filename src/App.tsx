ef6b2d68fe1edd3f7b921f327fdff045ace8475a
cball <cball@netchexonline.com>
2023-01-30 16:14:16 -0600

    dependency updates

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Hello, HelloClass } from './components';
 
 function App() {
   return (
-    <div className="m-auto">
+    <div className="m-auto w-full h-full flex justify-center flex-col items-center">
       <div className="flex space-x-4">
         <Hello text="I'm a Functional Component!" />
         <HelloClass text="I'm a Class Component!" />
